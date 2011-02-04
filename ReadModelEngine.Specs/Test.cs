2266448ef40b930fa7f9ee8928c2e5d99995edf7
Szymon Pobiega <szymon@pobiega.com>
2011-02-03 21:09:28 -0500

    Added reference to NUnit

--- ReadModelEngine.Specs/Test.cs
+++ ReadModelEngine.Specs/Test.cs
@@ -15,6 +15,14 @@ namespace ManagedViewEngine
             engine.GetSingletonView<SalesSummaryView>();
         }
 
+        public void After_calling_Initialize_not_initialized_views_should_not_be_available()
+        {
+            IViewEngine engine = null;
+            engine.Initialize(new[] { typeof(CustomerListView)});
+
+            engine.GetView<CustomerDetailsView>();
+        }
+
         public void Load_should_return_instance_of_view_if_present()
         {
             IViewEngine engine = null;
---
+++ ReadModelEngine/SqlPersistence/SqlViewEngine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ManagedViewEngine.SqlPersistence
+{
+    public class SqlViewEngine : IViewEngine
+    {
+        public void Initialize(IEnumerable<Type> viewCollection)
+        {
+        }
+
+        public IView<T> GetView<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISingletonView<T> GetSingletonView<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
